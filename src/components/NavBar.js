973646606ddfd181c8a358866785df8d689dbddd
hudahamid <huda.00091581@gmail.com>
2022-12-07 22:08:06 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+                {links.map((link,index)=>{
+                 return( <a href={`#${link}`} key={index}>{link}</a>)
+                }
+                )
+                }
+                {/* <a href="#home">home</a>
+                <a href="#about">about</a>
+                <a href="#projects">projects</a> */}
+  
+  
+  </nav>;
 }
 
 export default NavBar;
